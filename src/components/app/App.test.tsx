f28fe1f2402634d600bda9725442a03e45b49045
Yosvel Quintero <yosvelquintero@gmail.com>
2024-10-02 18:20:49 +0700

    refactor: Integrate invite guests feature and remove unnecessary tests

--- src/components/app/App.test.tsx
+++ src/components/app/App.test.tsx
@@ -8,17 +8,3 @@ test('renders "React App"', () => {
   const headingElement = screen.getByRole('heading', { name: 'React App' });
   expect(headingElement).toBeInTheDocument();
 });
-
-test('renders "Edit src/components/app/App.tsx and save to reload."', () => {
-  render(<App />);
-  const pElement = screen.getByText((_, element) => {
-    return element?.textContent === 'Edit src/components/app/App.tsx and save to reload.';
-  });
-  expect(pElement).toBeInTheDocument();
-});
-
-test('renders "Learn React" link', () => {
-  render(<App />);
-  const linkElement = screen.getByRole('link', { name: 'Learn React' });
-  expect(linkElement).toBeInTheDocument();
-});
--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,21 +1,14 @@
+import InviteGuests from '../invite-guests/InviteGuests';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
     <div className='container'>
       <h1 className='logo'>React App</h1>
 
-      <p>
-        Edit <code>src/components/app/App.tsx</code> and save to reload.
-      </p>
-
-      <p>
-        <a className='link' href='https://reactjs.org' target='_blank' rel='noopener noreferrer'>
-          Learn React
-        </a>
-      </p>
+      <InviteGuests />
     </div>
   );
-}
+};
 
 export default App;
