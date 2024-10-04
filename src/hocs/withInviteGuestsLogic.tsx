af32da87b33171ccbc76eafa62ee499f152bfc33
Yosvel Quintero <yosvelquintero@gmail.com>
2024-10-04 11:44:42 +0700

    refactor: Update withInviteGuestsLogic hoc to use a more descriptive type name

--- src/hocs/withInviteGuestsLogic.tsx
+++ src/hocs/withInviteGuestsLogic.tsx
@@ -2,11 +2,12 @@ import React from 'react';
 import useEmailManagement from '../hooks/useEmailManagement';
 
 type TInjectedProps = ReturnType<typeof useEmailManagement>;
+type TWithoutInjectedProps<P> = Omit<P, keyof TInjectedProps>;
 
 const withInviteGuestsLogic = <P extends object>(
   Component: React.ComponentType<P>
-): React.FC<Omit<P, keyof TInjectedProps>> => {
-  const WrappedComponent = (props: Omit<P, keyof TInjectedProps>) => {
+): React.FC<TWithoutInjectedProps<P>> => {
+  const WrappedComponent = (props: TWithoutInjectedProps<P>) => {
     const emailManagement = useEmailManagement();
     return <Component {...(props as P)} {...emailManagement} />;
   };
